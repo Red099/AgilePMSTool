720b02bb8b8fc582ac9d56790f4e6fc6d2844f49
Anand Shimpi <anand02cs004@gmail.com>
2020-12-27 23:41:34 +0530

    First Commit

---
+++ apmtool/src/main/java/io/agileinteligence/apmtool/ApmtoolApplication.java
@@ -0,0 +1,13 @@
+package io.agileinteligence.apmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmtoolApplication.class, args);
+	}
+
+}
---
+++ apmtool/src/test/java/io/agileinteligence/apmtool/ApmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileinteligence.apmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
